d178170d2f6a4eae625eb3420d4eea6b8745e446
keagnon <keagnon.gbe@gmail.com>
2023-02-05 12:20:12 +0100

    Adding social media icons and designing the image that represents me

--- portofolio-gbe/src/components/Header/CTA.jsx
+++ portofolio-gbe/src/components/Header/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <div className='cta'>
         <a href={cv} download className='btn'>Download CV</a>
-        <a href='#contact' className='btn btn-primary'>Talk with me</a>
+        <a href='#contact' className='btn btn-primary'>Talk with me </a>
 
     </div>
   )

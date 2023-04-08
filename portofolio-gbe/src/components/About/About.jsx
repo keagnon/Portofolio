6d54ae7a0e6ea963badce4e06cb7d05bdf08f54d
keagnon <keagnon.gbe@gmail.com>
2023-04-08 16:28:42 +0200

    Ajout section concernant la section A propos : Terminer

--- portofolio-gbe/src/components/About/About.jsx
+++ portofolio-gbe/src/components/About/About.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
 import './About.css'
+import ME from '../../assets/1.png'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderActive} from 'react-icons/vsc'
+
 const About = () => {
   return (
-    <section id='About'>About</section>
+    <section id='About'>
+      <h5 className='centrer_text'>About</h5>
+      <h3 className='centrer_text'>Get to know me</h3>
+
+      <div className="container about__container centrer">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="" />
+          </div>
+        </div>
+
+      <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__card_icon'/> 
+              <h5>Expérience</h5>
+              <small>+1 Années</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__card_icon'/> 
+              <h5>compétences</h5>
+              <small>+10 compétences(data sciences & Développement web)</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderActive className='about__card_icon'/> 
+              <h5>Projets</h5>
+              <small>+6 Terminé</small>
+            </article>
+            
+          </div>
+
+          <p>
+          Je suis très curieuse et passionnée par les mathématiques, la programmation ainsi que l’analyse des données. J’aime apprendre et j’occupe mon temps libre à m’auto-former et à lire des romans anglais .
+
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>M'envoyer un message</a>
+      </div>
+
+
+      </div>
+
+    </section>
   )
 }
 
--- portofolio-gbe/src/components/Experience/Experience.jsx
+++ portofolio-gbe/src/components/Experience/Experience.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 import './Experience.css'
 const Experience = () => {
   return (
-    <section id='Experience'>experience</section>
+    <section id='Experience'>
+      <h5>What Skills i have</h5>
+      <h2>My Expérience</h2>
+
+      <div className='container experience__container'>
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+           <article className='experience__details'>
+        
+            </article> 
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
